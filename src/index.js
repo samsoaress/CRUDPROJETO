7d82565a96ea86365568a952fde086f3a5e9d535
samsoaress <samuelsoaressprogramador@gmail.com>
2022-06-22 11:22:45 -0300

    atulização do formulario e cpf e cnpj

--- src/index.js
+++ src/index.js
@@ -33,11 +33,13 @@ function mtel(v) {
   return v;
 }
 function cpf(v){
-if (v.length <= 11) {
+if (v.length <= 14) {
+  v = v.replace(/\D/g, "")
   v = v.replace(/(\d{3})(\d)/, "$1.$2")
   v = v.replace(/(\d{3})(\d)/, "$1.$2")
   v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
 } else {
+  v = v.replace(/\D/g, "")
   v = v.replace(/^(\d{2})(\d)/, "$1.$2")
   v = v.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
   v = v.replace(/\.(\d{3})(\d)/, ".$1/$2")
